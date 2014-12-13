b7cd7fcd0c53722134ff4330b24d898045d7299f
Vítězslav Čížek <civ@blema.cz>
2014-12-13 20:10:34 +0100

    skeleton file for set 5

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use set1::chals::{chal1,chal2,chal3,chal4,chal5,chal6,chal7,chal8};
 use set2::chals::{chal9,chal10,chal11,chal12,chal13,chal14,chal15,chal16};
 use set3::chals::{chal17,chal18,chal19,chal20,chal21,chal22,chal23,chal24};
 use set4::chals::{chal25,chal26,chal27,chal28,chal29,chal30,chal31,chal32};
+use set5::chals::{chal33,chal34,chal35,chal36,chal37,chal38,chal39,chal40};
 
 mod utils;
 mod set1;
@@ -45,6 +46,8 @@ fn main() {
 	chal27();
 	chal28();
 	chal31();
-*/
 	chal32();
+*/
+	// set 5
+	chal33();
 }
---
+++ src/set5/chals.rs
@@ -0,0 +1,42 @@
+// Implement Diffie-Hellman
+pub fn chal33() {
+	//TODO
+}
+
+// Implement a MITM key-fixing attack on Diffie-Hellman with parameter injection
+pub fn chal34() {
+	//TODO
+}
+
+// Implement DH with negotiated groups, and break with malicious "g" parameters
+pub fn chal35() {
+	//TODO
+}
+
+// Implement Secure Remote Password (SRP)
+pub fn chal36() {
+	//TODO
+}
+
+// Break SRP with a zero key
+pub fn chal37() {
+	//TODO
+}
+
+// Offline dictionary attack on simplified SRP
+pub fn chal38() {
+	//TODO
+}
+
+// Implement RSA
+pub fn chal39() {
+	//TODO
+}
+
+// Implement an E=3 RSA Broadcast attack
+pub fn chal40() {
+	//TODO
+}
+
+
+
